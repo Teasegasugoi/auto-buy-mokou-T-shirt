a35474e3e053f2b17bd1e84c003ad94ffaac18c2
shuhei izaki <zakipr.roni29@gmail.com>
2022-08-20 05:47:25 +0900

    Seleniumの動作テストコード

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+import chromedriver_binary # nopa
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome import service as fs
 
-
-CHROMEDRIVER = '/opt/chrome/chromedriver'
-URL = '{スクレイピングするURLを記載}'
-
-options = Options()
+# x. Chrome の起動オプションを設定する
+options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
 
-chrome_service = fs.Service(executable_path=CHROMEDRIVER)
-driver = webdriver.Chrome(service=chrome_service, options=options)
-driver.get(URL)
-html = driver.page_source
-print(html)
+# x. ブラウザの新規ウィンドウを開く
+print('connecting to remote browser...')
+driver = webdriver.Chrome(options=options)
+
+# 1. Qiita にアクセスする
+driver.get('https://qiita.com')
+print(driver.current_url)
+
+# x. ブラウザを終了する
+driver.quit()
